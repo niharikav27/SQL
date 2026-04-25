92ccbdfa9294c89ebe854cfbd15a6f283e2e4cbc
niharikav27 <neerjav80@gmail.com>
2026-04-25 21:38:08 +0530

    initial commit

---
+++ 7select.sql
@@ -0,0 +1,7 @@
+USE instagram;
+
+SELECT id,age,email FROM user;     -- select given column
+
+SELECT * FROM user;       -- give all columns
+
+SELECT DISTINCT AGE FROM user;       -- give unique values
